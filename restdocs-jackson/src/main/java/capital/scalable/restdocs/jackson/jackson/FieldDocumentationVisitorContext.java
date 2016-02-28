ff8ae97353c5dde5e60607810ed3668842c0ccb3
Florian Benz <florianbenz1@gmail.com>
2016-02-28 09:48:26 +0100

    Copyright headers

--- restdocs-jackson/src/main/java/capital/scalable/restdocs/jackson/jackson/FieldDocumentationVisitorContext.java
+++ restdocs-jackson/src/main/java/capital/scalable/restdocs/jackson/jackson/FieldDocumentationVisitorContext.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package capital.scalable.restdocs.jackson.jackson;
 
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -14,6 +30,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.util.StringUtils;
 
+/**
+ * @author Florian Benz
+ */
 public class FieldDocumentationVisitorContext {
 
     private static final Logger log = LoggerFactory.getLogger(FieldDocumentationVisitorContext.class);
