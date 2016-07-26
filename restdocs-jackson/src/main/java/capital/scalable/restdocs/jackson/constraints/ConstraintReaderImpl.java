783f7afa897296eb77d9a5b8905549e0a22507a0
Juraj Misur <juraj.misur@gmail.com>
2016-07-26 21:53:14 +0200

    cleanup

--- restdocs-jackson/src/main/java/capital/scalable/restdocs/jackson/constraints/ConstraintReaderImpl.java
+++ restdocs-jackson/src/main/java/capital/scalable/restdocs/jackson/constraints/ConstraintReaderImpl.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.core.MethodParameter;
@@ -11,25 +12,23 @@ import org.springframework.restdocs.constraints.ConstraintDescriptions;
 import org.springframework.restdocs.constraints.ValidatorConstraintResolver;
 
 public class ConstraintReaderImpl implements ConstraintReader {
-    public static final List<Class<?>> MANDATORY_VALUE_ANNOTATIONS;
+    // TODO support for composed constraints
+    public static final Class<?>[] MANDATORY_VALUE_ANNOTATIONS =
+            {NotNull.class, NotEmpty.class, NotBlank.class};
 
-    static {
-        MANDATORY_VALUE_ANNOTATIONS = new ArrayList<>();
-        MANDATORY_VALUE_ANNOTATIONS.add(NotNull.class);
-        MANDATORY_VALUE_ANNOTATIONS.add(NotEmpty.class);
-        MANDATORY_VALUE_ANNOTATIONS.add(NotBlank.class);
-    }
+    private SkippableConstraintResolver constraintResolver =
+            new SkippableConstraintResolver(new ValidatorConstraintResolver(),
+                    MANDATORY_VALUE_ANNOTATIONS);
 
     @Override
     public boolean isMandatory(Class<?> annotation) {
-        return MANDATORY_VALUE_ANNOTATIONS.contains(annotation);
+        return ArrayUtils.contains(MANDATORY_VALUE_ANNOTATIONS, annotation);
     }
 
     @Override
     public List<String> getConstraintMessages(Class<?> javaBaseClass, String javaFieldName) {
         ConstraintDescriptions constraints = new ConstraintDescriptions(javaBaseClass,
-                new SkippableConstraintResolver(new ValidatorConstraintResolver(),
-                        MANDATORY_VALUE_ANNOTATIONS));
+                constraintResolver);
         return constraints.descriptionsForProperty(javaFieldName);
     }
 
