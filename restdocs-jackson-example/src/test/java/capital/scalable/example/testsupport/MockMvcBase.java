bb104e698f3c79f06078fe4a64a14e5afcbe7d65
Florian Benz <florianbenz1@gmail.com>
2016-09-11 19:08:33 +0200

    Use forgotten property

--- restdocs-jackson-example/src/test/java/capital/scalable/example/testsupport/MockMvcBase.java
+++ restdocs-jackson-example/src/test/java/capital/scalable/example/testsupport/MockMvcBase.java
@@ -89,7 +89,8 @@ public abstract class MockMvcBase {
 
     @Rule
     public final JUnitRestDocumentation restDocumentation =
-            new JUnitRestDocumentation("target/generated-snippets");
+            new JUnitRestDocumentation(
+                    System.getProperties().getProperty("org.springframework.restdocs.outputDir"));
 
     @Before
     public void setUp() throws Exception {
