3c13f2f117db089b225c6ab68b23df12351b67c2
Florian Benz <florianbenz1@gmail.com>
2017-02-05 15:55:28 +0100

    Remove unused import

--- spring-auto-restdocs-example/src/main/java/capital/scalable/restdocs/example/security/WebSecurityConfiguration.java
+++ spring-auto-restdocs-example/src/main/java/capital/scalable/restdocs/example/security/WebSecurityConfiguration.java
@@ -24,7 +24,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
