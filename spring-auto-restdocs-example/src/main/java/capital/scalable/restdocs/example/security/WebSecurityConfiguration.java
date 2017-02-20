0dabbde7ef95a578da018884740bccceb8dbb371
Florian Benz <florianbenz1@gmail.com>
2017-02-20 11:52:57 +0100

    Update of dependencies (#59)
    * Update of dependencies
    
    * Remove unused import
    
    * Use same version of Hibernate Validator as latest Spring Boot

--- spring-auto-restdocs-example/src/main/java/capital/scalable/restdocs/example/security/WebSecurityConfiguration.java
+++ spring-auto-restdocs-example/src/main/java/capital/scalable/restdocs/example/security/WebSecurityConfiguration.java
@@ -21,12 +21,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
@@ -37,9 +38,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+
+    @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
 }
