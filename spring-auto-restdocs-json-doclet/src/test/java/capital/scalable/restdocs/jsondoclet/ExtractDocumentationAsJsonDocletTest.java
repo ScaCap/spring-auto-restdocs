3227c327d9a8edacf2210992d32b5f55aabf5638
Florian Benz <florianbenz1@gmail.com>
2017-02-20 11:53:21 +0100

    Fix #69 - Ensure that Javadoc JSON files are always UTF-8 encoded (#73)

--- spring-auto-restdocs-json-doclet/src/test/java/capital/scalable/restdocs/jsondoclet/ExtractDocumentationAsJsonDocletTest.java
+++ spring-auto-restdocs-json-doclet/src/test/java/capital/scalable/restdocs/jsondoclet/ExtractDocumentationAsJsonDocletTest.java
@@ -16,6 +16,8 @@
 
 package capital.scalable.restdocs.jsondoclet;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -30,12 +32,16 @@ public class ExtractDocumentationAsJsonDocletTest {
     private static final String JSON_PATH =
             "capital/scalable/restdocs/jsondoclet/DocumentedClass.json";
 
+    /**
+     * The test requires that the Doclet is executed before. This is ensured by
+     * the Maven configuration, but not when the test is executed on its own.
+     */
     @Test
     public void testDocumentedClass() throws IOException, JSONException {
         String generated = IOUtils.toString(
-                new FileInputStream(new File("target/generated-javadoc-json/" + JSON_PATH)));
+                new FileInputStream(new File("target/generated-javadoc-json/" + JSON_PATH)), UTF_8);
         String expected = IOUtils.toString(
-                this.getClass().getClassLoader().getResourceAsStream(JSON_PATH));
+                this.getClass().getClassLoader().getResourceAsStream(JSON_PATH), UTF_8);
         JSONAssert.assertEquals(expected, generated, false);
     }
 }
