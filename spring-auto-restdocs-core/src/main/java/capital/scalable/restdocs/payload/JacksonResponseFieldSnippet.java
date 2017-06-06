1ed2fcbe61d3541d67a79ee7a24112a1de0a4233
lyashenkogs <lyashenkogs@gmail.com>
2017-06-06 22:58:58 +0100

    Add support for HttpEntity.class

--- spring-auto-restdocs-core/src/main/java/capital/scalable/restdocs/payload/JacksonResponseFieldSnippet.java
+++ spring-auto-restdocs-core/src/main/java/capital/scalable/restdocs/payload/JacksonResponseFieldSnippet.java
@@ -23,6 +23,7 @@ import java.lang.reflect.Type;
 import java.util.Map;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.method.HandlerMethod;
 
@@ -37,7 +38,11 @@ public class JacksonResponseFieldSnippet extends AbstractJacksonFieldSnippet {
         Class<?> returnType = method.getReturnType().getParameterType();
         if (returnType == ResponseEntity.class) {
             return firstGenericType(method.getReturnType());
-        } else if (returnType == Page.class) {
+        }
+        else if(returnType == HttpEntity.class){
+            return firstGenericType(method.getReturnType());
+        }
+        else if (returnType == Page.class) {
             return firstGenericType(method.getReturnType());
         } else if (isCollection(returnType)) {
             return new GenericArrayType() {
