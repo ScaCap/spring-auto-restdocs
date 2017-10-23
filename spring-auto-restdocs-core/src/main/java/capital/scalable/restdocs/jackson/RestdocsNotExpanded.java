7d3a1f5e46a39456c62b49760821500b5c4fdbbc
Juraj Misur <juraj.misur@gmail.com>
2017-10-23 12:59:55 +0200

    Optional validation (#149)
    * Add example without validation
    
    * Optionally use javax.validation
    
    * Add example of the generated documentation without validation
    
    * cleaned up and added tests
    
    * removed validation api from pom
    
    * removed reformatting of html

--- spring-auto-restdocs-core/src/main/java/capital/scalable/restdocs/jackson/RestdocsNotExpanded.java
+++ spring-auto-restdocs-core/src/main/java/capital/scalable/restdocs/jackson/RestdocsNotExpanded.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package capital.scalable.restdocs.jackson;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
@@ -7,7 +23,6 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import javax.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -21,6 +36,4 @@ public @interface RestdocsNotExpanded {
     String message() default "";
 
     Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
 }
