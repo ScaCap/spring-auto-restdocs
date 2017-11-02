3c59829a688d562f0a8ac1c1c6aebf68025d1eed
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2017-11-02 22:46:18 +0000

    New snippet for @RequestHeaders parameters

---
+++ spring-auto-restdocs-core/src/main/java/capital/scalable/restdocs/request/RequestHeaderSnippet.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package capital.scalable.restdocs.request;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+public class RequestHeaderSnippet extends AbstractParameterSnippet<RequestHeader> {
+
+    public static final String REQUEST_HEADERS = "auto-request-headers";
+    private final boolean failOnUndocumentedParams;
+
+    public RequestHeaderSnippet() {
+        this(false);
+    }
+
+    public RequestHeaderSnippet(boolean failOnUndocumentedParams) {
+        super(REQUEST_HEADERS, null);
+        this.failOnUndocumentedParams = failOnUndocumentedParams;
+    }
+
+    public RequestHeaderSnippet failOnUndocumentedParams(boolean failOnUndocumentedParams) {
+        return new RequestHeaderSnippet(failOnUndocumentedParams);
+    }
+
+    @Override
+    protected boolean isRequired(MethodParameter param, RequestHeader annot) {
+        return param.getParameterType().isPrimitive()
+                ? true // spring disallows null for primitive
+                : annot.required();
+    }
+
+    @Override
+    protected String getPath(RequestHeader annot) {
+        return annot.value();
+    }
+
+    protected RequestHeader getAnnotation(MethodParameter param) {
+        return param.getParameterAnnotation(RequestHeader.class);
+    }
+
+    @Override
+    public String getHeader() {
+        return "Request headers";
+    }
+
+    @Override
+    protected boolean shouldFailOnUndocumentedParams() {
+        return failOnUndocumentedParams;
+    }
+}
