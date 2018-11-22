723aea4c914759dc77f61f8feccdccd0544ee62b
Juraj Misur <juraj.misur@gmail.com>
2018-11-22 09:59:58 +0100

    improvements

--- spring-auto-restdocs-core/src/main/java/capital/scalable/restdocs/postman/PostmanRule.java
+++ spring-auto-restdocs-core/src/main/java/capital/scalable/restdocs/postman/PostmanRule.java
@@ -1,7 +1,29 @@
+/*-
+ * #%L
+ * Spring Auto REST Docs Core
+ * %%
+ * Copyright (C) 2015 - 2018 Scalable Capital GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package capital.scalable.restdocs.postman;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+
+import java.io.File;
+import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,9 +32,15 @@ import org.junit.rules.ExternalResource;
 
 public class PostmanRule extends ExternalResource {
     private final PostmanCollection postmanCollection;
+    private final File outputDirectory;
 
     public PostmanRule(String name, String description) {
+        this(name, description, getDefaultOutputDirectory());
+    }
+
+    public PostmanRule(String name, String description, String outputDirectory) {
         this.postmanCollection = new PostmanCollection(name, description);
+        this.outputDirectory = new File(outputDirectory);
     }
 
     public PostmanCollection get() {
@@ -24,11 +52,13 @@ public class PostmanRule extends ExternalResource {
         ObjectMapper objectMapper = setupMapper();
         String json = null;
         try {
-            json = objectMapper.writerFor(PostmanCollection.class).writeValueAsString(postmanCollection);
+            File file = new File(outputDirectory, "postman.json");
+            objectMapper.writerFor(PostmanCollection.class).writeValue(file, postmanCollection);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(json);
     }
 
     private ObjectMapper setupMapper() {
@@ -41,5 +71,12 @@ public class PostmanRule extends ExternalResource {
                         .withCreatorVisibility(NONE));
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         return objectMapper;
+    }
+
+    private static String getDefaultOutputDirectory() {
+        if (new File("pom.xml").exists()) {
+            return "target/generated-snippets";
+        }
+        return "build/generated-snippets";
     }
 }
