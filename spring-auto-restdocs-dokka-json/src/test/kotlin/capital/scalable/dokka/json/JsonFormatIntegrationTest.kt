2e9b4eb71aa8a2455a86a541b2cc645c64bc5fcf
Florian Benz <florian.benz@scalable.capital>
2020-04-13 17:11:20 +0200

    Fix dependency injection issue and cover it with a test

--- spring-auto-restdocs-dokka-json/src/test/kotlin/capital/scalable/dokka/json/JsonFormatIntegrationTest.kt
+++ spring-auto-restdocs-dokka-json/src/test/kotlin/capital/scalable/dokka/json/JsonFormatIntegrationTest.kt
@@ -20,72 +20,36 @@
 package capital.scalable.dokka.json
 
 import com.google.inject.Binder
-import com.google.inject.Module
 import com.google.inject.Guice
+import com.google.inject.Module
 import com.google.inject.name.Names
-import org.jetbrains.dokka.Utilities.ServiceLocator
-import org.junit.Test
-import org.jetbrains.dokka.Formats.FormatDescriptor
-import org.jetbrains.dokka.DokkaLogger
-import org.jetbrains.dokka.DokkaConsoleLogger
-import org.jetbrains.dokka.DocumentationNode
-import org.jetbrains.dokka.Content
-import org.jetbrains.dokka.NodeKind
-import java.io.File
 import com.intellij.openapi.util.io.FileUtil
+import org.jetbrains.dokka.Content
+import org.jetbrains.dokka.DefaultPackageListService
+import org.jetbrains.dokka.DocumentationNode
 import org.jetbrains.dokka.DokkaConfiguration
-import org.jetbrains.dokka.Platform
-import org.jetbrains.dokka.Utilities.DokkaRunModule
+import org.jetbrains.dokka.DokkaConsoleLogger
+import org.jetbrains.dokka.DokkaGenerator
+import org.jetbrains.dokka.DokkaLogger
 import org.jetbrains.dokka.FormatService
-
+import org.jetbrains.dokka.Formats.FormatDescriptor
 import org.jetbrains.dokka.Generator
 import org.jetbrains.dokka.KotlinLanguageService
 import org.jetbrains.dokka.LanguageService
+import org.jetbrains.dokka.NodeKind
 import org.jetbrains.dokka.NodeLocationAwareGenerator
 import org.jetbrains.dokka.PackageListService
-import org.jetbrains.dokka.DefaultPackageListService
+import org.jetbrains.dokka.Platform
+import org.jetbrains.dokka.Utilities.DokkaRunModule
+import org.jetbrains.dokka.Utilities.ServiceLocator
+import org.junit.Test
+import java.io.File
 
-import org.jetbrains.dokka.DokkaGenerator
-
-class JsonFormatDescriptor2Test {
+class JsonFormatIntegrationTest {
 
     @Test
     fun `Should load descriptor from properties file, bind and use it`() {
         DokkaGenerator(DokkaConfigurationImpl(), DokkaConsoleLogger).generate()
-    }
-
-    @Test
-    fun `Should load descriptor from properties file and bind it correctly x`() {
-        println("Start")
-
-        val globalInjector = Guice.createInjector(DokkaRunModule(DokkaConfigurationImpl()))
-        val injector = globalInjector.createChildInjector(JsonFormatDescriptor2TestModule())
-
-        val formatService = injector.getInstance(FormatService::class.java)
-        println("1 $formatService")
-
-        val jsonFormatService = injector.getInstance(JsonFormatService::class.java)
-        println("2 $jsonFormatService")
-
-        val jsonFileGenerator = injector.getInstance(JsonFileGenerator::class.java)
-        val x = jsonFileGenerator.location(DocumentationNode("", Content.Empty, NodeKind.Parameter))
-        val y = x.path.contains("format-descriptor-test")
-        println("$x - $y")
-    }
-}
-
-class JsonFormatDescriptor2TestModule(): Module {
-    override fun configure(binder: Binder) {
-        //val outputDir: File = FileUtil.createTempDirectory("dokka-json", "format-descriptor-test")
-        //binder.bind(File::class.java).annotatedWith(Names.named("outputDir")).toInstance(outputDir)
-        binder.bind(DokkaLogger::class.java).toInstance(DokkaConsoleLogger)
-
-        /*binder.bind(Generator::class.java).to(NodeLocationAwareGenerator::class.java)
-        binder.bind(NodeLocationAwareGenerator::class.java).to(JsonFileGenerator::class.java)
-        binder.bind(JsonFileGenerator::class.java) // https://github.com/google/guice/issues/847
-        binder.bind(LanguageService::class.java).to(KotlinLanguageService::class.java)
-        binder.bind(FormatService::class.java).to(JsonFormatService::class.java)
-        binder.bind(PackageListService::class.java).to(DefaultPackageListService::class.java)*/
     }
 }
 
@@ -140,7 +104,7 @@ class PassConfigurationImpl (
     override val jdkVersion: Int = 6,
     override val sourceLinks: List<DokkaConfiguration.SourceLinkDefinition> = emptyList(),
     override val perPackageOptions: List<DokkaConfiguration.PackageOptions> = emptyList(),
-    externalDocumentationLinks: List<DokkaConfiguration.ExternalDocumentationLink> = emptyList(),
+    override val externalDocumentationLinks: List<DokkaConfiguration.ExternalDocumentationLink> = emptyList(),
     override val languageVersion: String? = null,
     override val apiVersion: String? = null,
     override val noStdlibLink: Boolean = false,
@@ -150,15 +114,4 @@ class PassConfigurationImpl (
     override val analysisPlatform: Platform = Platform.DEFAULT,
     override val targets: List<String> = emptyList(),
     override val sinceKotlin: String? = null
-): DokkaConfiguration.PassConfiguration {
-    private val defaultLinks = run {
-        val links = mutableListOf<DokkaConfiguration.ExternalDocumentationLink>()
-        if (!noJdkLink)
-            links += DokkaConfiguration.ExternalDocumentationLink.Builder("https://docs.oracle.com/javase/$jdkVersion/docs/api/").build()
-
-        if (!noStdlibLink)
-            links += DokkaConfiguration.ExternalDocumentationLink.Builder("https://kotlinlang.org/api/latest/jvm/stdlib/").build()
-        links
-    }
-    override val externalDocumentationLinks = defaultLinks + externalDocumentationLinks
-}
+): DokkaConfiguration.PassConfiguration
