a8759a5a83cce1bdc2593114df1160fe54657c23
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2021-02-02 20:25:33 +0100

    updated copyright

--- spring-auto-restdocs-json-doclet-jdk9/src/main/java/capital/scalable/restdocs/jsondoclet/DocletAbortException.java
+++ spring-auto-restdocs-json-doclet-jdk9/src/main/java/capital/scalable/restdocs/jsondoclet/DocletAbortException.java
@@ -2,7 +2,7 @@
  * #%L
  * Spring Auto REST Docs Json Doclet for JDK9+
  * %%
- * Copyright (C) 2015 - 2020 Scalable Capital GmbH
+ * Copyright (C) 2015 - 2021 Scalable Capital GmbH
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
